dff16944efe043a346d9b2d238f5a18368f9c021
Abraxas-365 <luisfmiranda8@gmail.com>
2023-12-27 13:57:37 -0500

    feat(main): adding clab

--- src/main.rs
+++ src/main.rs
@@ -4,27 +4,56 @@ mod client;
 mod clipboard_listener;
 mod server;
 
-use std::env;
 use std::process;
 
 use crate::client::Client;
 use crate::server::Server;
+use clap::{App, Arg};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Remote Clipboard Sync")
+        .version("1.0")
+        .author("Your Name <your_email@example.com>")
+        .about("Synchronizes clipboard contents between a remote server and a local machine.")
+        .arg(
+            Arg::with_name("role")
+                .help("The role to run ('server' or 'client')")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("address")
+                .help("The address to bind/connect to")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("port")
+                .help("The port to bind/connect to")
+                .required(true)
+                .index(3),
+        )
+        .arg(
+            Arg::with_name("clipboard_file")
+                .help("The path to the clipboard file (only required for server)")
+                .required(false)
+                .index(4),
+        )
+        .get_matches();
 
-    if args.len() != 5 {
-        eprintln!("Usage: program [server|client] <address> <port> [clipboard_file]");
-        process::exit(1);
-    }
+    let role = matches.value_of("role").unwrap();
+    let address = matches.value_of("address").unwrap();
+    let port = matches
+        .value_of("port")
+        .unwrap()
+        .parse::<u16>()
+        .expect("Invalid port number");
 
-    let role = &args[1];
-    let address = &args[2];
-    let port = args[3].parse::<u16>().expect("Invalid port number");
-
-    match role.as_str() {
+    match role {
         "server" => {
-            let clipboard_file = &args[4];
+            let clipboard_file = matches
+                .value_of("clipboard_file")
+                .expect("Clipboard file is required for server");
             let server = Server::new(address, port, clipboard_file)
                 .expect("Failed to create server with given address and port");
             server.run()?;
